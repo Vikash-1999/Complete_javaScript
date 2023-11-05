d20a9d8668c2753c9ef3098aab1c6f7627998e83
Vikash-1999 <133888217+Vikash-1999@users.noreply.github.com>
2023-11-05 11:52:46 +0000

    operations

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -27,6 +27,51 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let data =  33
 let numberData = String(data)
-console.log(typeof numberData)
-console.log(numberData)
+// console.log(typeof numberData)
+// console.log(numberData)
+
+// *********************** Operations *************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**4)// => power
+// console.log(2%2)
+
+let str1 = "Vikas"
+let str2 = " Hello"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(typeof ("1" + 2))
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true); // output => 1
+// console.log(+""); // output => 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter)
+
+let myNumber = 12;
+console.log(myNumber++);// => 12 | print 12 then increase by 1
+console.log(myNumber--);// => 13 | print increased (13) then deacresed by 1
+console.log(++myNumber);// => 13 | increase the value by 1 then print
+console.log(--myNumber);// => 12 | deacrease the value by 1 then print
+
+
+
+
 
