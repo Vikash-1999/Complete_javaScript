2812e2154c7a5243a6e277175b51535c09691895
Vikash-1999 <133888217+Vikash-1999@users.noreply.github.com>
2023-11-05 11:17:46 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,32 @@
+let score = "3"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )// => number
+// console.log(valueInNumber)
+
+let life = null
+let lifeInNumber = Number(life)
+// console.log(typeof lifeInNumber)
+// console.log(lifeInNumber)
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false ; "vikash" => true
+// 
+
+let data =  33
+let numberData = String(data)
+console.log(typeof numberData)
+console.log(numberData)
+
