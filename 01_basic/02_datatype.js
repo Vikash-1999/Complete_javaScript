2f4c35efb81efc58372cdd7b150e7aa69e606641
Vikash-1999 <133888217+Vikash-1999@users.noreply.github.com>
2023-11-05 10:53:40 +0000

    datatype in js

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all js code as newer version
+
+//  alert( 3 + 3)  // we are using nodejs, not browser
+
+// console.log(3 + 3)// code readability should high
+
+// console.log("vikash")
+
+
+let name = "Vikash"
+let age = 15
+let isLoggedIn = false
+
+// number => 2 , 3 , 4, -3, 0.1......
+// string => "2" // it is a string
+// boolean => true/false
+// null => it is a data type and stand alone value
+// undefined => unassigned value
+// symbol => unique value
+
+// object
+
+console.log(typeof null) // important null is object datatype
+console.log(typeof undefined)// undefined is undefined datatype
+
+
+
+
+
