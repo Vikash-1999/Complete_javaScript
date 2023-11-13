41e5046cb2ef6f3a314c48f1d69b3c91c8efe30e
Vikash-1999 <133888217+Vikash-1999@users.noreply.github.com>
2023-11-13 11:21:31 +0000

    Nums and Math

---
+++ 01_basic/07_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-In'));
+
+// **********************  Maths  *******************
+
+// console.log(Maths);
+// console.log(Maths.round(4.6));
+// console.log(Maths.abs(-4));
+// console.log(Maths.ceil(4.2));
+// console.log(Maths.floor(4.9));
+// console.log(Maths.min(4, 3, 5, 8));
+// console.log(Maths.max(4, 3, 5, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+
+
+
+
+
+
+
+
+
+
